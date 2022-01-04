65f843a1b8c860a31da921dd4939f2d7b7bf13cd
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-01-04 18:17:27 +0000

    Reorganise examples

--- primitives/main.rs
+++ primitives/main.rs
@@ -6,15 +6,9 @@ fn type_of<T>(_: T) -> &'static str {
 }
 
 fn main() {
-  //println!("1 {}", type_of(1));
   //println!("false {}", type_of(false));
 
-  //println!("\u{2705} {}", type_of('\u{2705}'));
-
-  /*
-  let a_float: f64 = 1.0;
-  println!("a_float {}", type_of(a_float));
-  */
+  //println!("1 {}", type_of(1));
 
   // println!("5i32 {}", type_of(5i32));
 
@@ -24,20 +18,25 @@ fn main() {
   */
 
   /*
+  let default_integer = 4;
+  println!("default_integer {} {}", default_integer, type_of(default_integer));
+  */
+
+  //println!("\u{2705} {}", type_of('\u{2705}'));
+
+  /*
   let default_float = 3.0;
   println!("default_float {} {}", default_float, type_of(default_float));
   */
 
   /*
-  let default_integer = 4;
-  println!("default_integer {} {}", default_integer, type_of(default_integer));
+  let a_float: f64 = 1.0;
+  println!("a_float {}", type_of(a_float));
   */
-
 
   /*
-  let mut inferred_type = 12; // Type i64 is inferred from another line
+  let mut inferred_type = -12; // Type i64 is inferred from another line
   println!("inferred_type (mutable) {} {}", inferred_type, type_of(inferred_type));
   */
-
-  //inferred_type = 5_000_000_000i64;
+  //inferred_type = 5_000_000_000u64;
 }
