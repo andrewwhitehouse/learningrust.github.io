e77b11b9815bb87987cff71b09e8417eceee07f7
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-01-08 12:39:53 +0000

    Missed a replacement

--- examples/tuples/main.rs
+++ examples/tuples/main.rs
@@ -3,6 +3,6 @@ fn main() {
     let point2 = (80, 92);
     let point1_delta = (point2.0 - point1.0) as f32;
     let point2_delta = (point2.1 - point1.1) as f32;
-    let length = (point1_delta*point1_diff + point2_diff*point2_diff).sqrt();
+    let length = (point1_delta*point1_delta + point2_delta*point2_delta).sqrt();
     println!("{}", length);
 }
