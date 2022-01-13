35895ab32679d10da89f91080f7bc30c06bf25dc
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-01-13 18:53:28 +0000

    First fizz test case.

--- examples/fizzbuzz/src/lib.rs
+++ examples/fizzbuzz/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn fizzbuzz(n: u64) -> String {
-  "".to_string()
+  if n == 3 { "fizz".to_string() } else { n.to_string() }
 }
 
 #[cfg(test)]
@@ -7,7 +7,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_returns_the_as_a_string() {
+    fn it_returns_one_as_a_string() {
         assert_eq!(fizzbuzz(1), "1");
+    }
+
+    #[test]
+    fn it_returns_two_as_a_string() {
+        assert_eq!(fizzbuzz(2), "2");
+    }
+
+    #[test]
+    fn it_returns_fizz_for_3() {
+        assert_eq!(fizzbuzz(3), "fizz");
     }
 }
