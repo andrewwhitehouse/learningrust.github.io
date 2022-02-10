651d3004548f1104e27baf0250d02bc0b2721eeb
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-02-10 18:52:45 +0000

    Failed conversion.

--- examples/ria_chapter2/comparing.rs
+++ examples/ria_chapter2/comparing.rs
@@ -1,7 +1,12 @@
+use std::convert::TryInto;
+
 fn main() {
     let a: i32 = 10;
-    let b: u16 = 100;
-    if a < (b as i32) {
+    let b: u128 = 10000000000;
+
+    let b_ = b.try_into().unwrap();
+
+    if a < b_ {
         println!("a is less than b");
     }
 }
